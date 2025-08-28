7c8adde724f8d09e42cbd8088e9e83774dd106f5
Khanh <khanhacademic723@gmail.com>
2025-08-28 10:56:37 +0700

    confligServlet

--- DinhQuocKhanh_21096081_WWW_T1/src/main/java/edu/iuh/fit/se/dinhquockhanh_www_t1/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.iuh.fit.se.dinhquockhanh_www_t1;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
