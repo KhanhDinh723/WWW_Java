097ecd163e23b307357f03dfdb80056f9f0d65a2
Khanh <khanhacademic723@gmail.com>
2025-09-18 18:10:24 +0700

    Update

---
+++ iocsample/src/main/java/edu/iuh/fit/se/iocsample/Student.java
@@ -0,0 +1,12 @@
+package edu.iuh.fit.se.iocsample;
+
+/**
+ * Author: Khánh Đinh
+ * Date:   9/14/2025
+ * Time:   4:46 PM
+ */
+public class Student {
+    private String id;
+    private String name;
+
+}
---
+++ iocsample/src/main/java/edu/iuh/fit/se/iocsample/main.java
@@ -0,0 +1,18 @@
+package edu.iuh.fit.se.iocsample;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Author: Khánh Đinh
+ * Date:   9/14/2025
+ * Time:   4:30 PM
+ */
+public class main {
+    public static void main(String[] args) {
+       ApplicationContext context = new ClassPathXmlApplicationContext(
+               Student student = context.getBean("student", Student.class);
+
+       )
+    }
+}
